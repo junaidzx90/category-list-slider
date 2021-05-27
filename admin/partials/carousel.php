39198f8de2af458683725d366e8d3b8bc9bac0e0
junaidzx90 <jsjunu@gmail.com>
2021-05-28 04:32:24 +0600

    5

--- admin/partials/carousel.php
+++ admin/partials/carousel.php
@@ -5,7 +5,15 @@
     foreach ($the_query->get_terms() as $term) {
         ?>
         <div class="slide">
-            <img src="<?php echo get_term_meta($term->term_id,'term_image',true); ?>" alt="term-img" />
+            <?php 
+            $thumbid = 0;
+            if($atts['taxonomy'] === 'product_cat'){
+                $thumbid = get_term_meta($term->term_id,'thumbnail_id',true); 
+            }else{
+                $thumbid = attachment_url_to_postid(get_term_meta($term->term_id, 'term_image', true));
+            }
+            ?>
+            <img src="<?php echo wp_get_attachment_url( $thumbid ) ?>" alt="term-img" />
             <div class="slideinfo">
                 <a href="<?php echo esc_url(get_term_link( $term->term_id )) ?>"><?php echo _e($term->name); ?></a>
             </div>
